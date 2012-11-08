c5b2d1e56f6a99c3945cbd8d555a3224e948f2d3
Chris Hardaker <chris.hardaker.de@gmail.com>
2012-11-09 00:06:10 +0100

    final, hopefully, initial upload

---
+++ BerlinGraffitiTests/BerlinGraffitiTests.h
@@ -0,0 +1,13 @@
+//
+//  BerlinGraffitiTests.h
+//  BerlinGraffitiTests
+//
+//  Created by Chris on 12.10.12.
+//  Copyright (c) 2012 BerliniODDev. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BerlinGraffitiTests : SenTestCase
+
+@end
---
+++ BerlinGraffitiTests/BerlinGraffitiTests.m
@@ -0,0 +1,32 @@
+//
+//  BerlinGraffitiTests.m
+//  BerlinGraffitiTests
+//
+//  Created by Chris on 12.10.12.
+//  Copyright (c) 2012 BerliniODDev. All rights reserved.
+//
+
+#import "BerlinGraffitiTests.h"
+
+@implementation BerlinGraffitiTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BerlinGraffitiTests");
+}
+
+@end
